24920bb2aa2f8bcdd8a8123ace6d503e044b3a7f
Łukasz Wiatrak <lukasz.wiatrak@gmail.com>
2014-03-03 17:56:09 +0100

    Teamcity sql provider fix

--- TrendsTracker.Tests.E2E/TestInfrastructure/E2ETestBase.cs
+++ TrendsTracker.Tests.E2E/TestInfrastructure/E2ETestBase.cs
@@ -27,5 +27,16 @@ namespace TrendsTracker.E2ETests
         {
             new DataDeleter().DeleteAllData();
         }
+
+        public void FixEfProviderServicesProblem()
+        {
+            //Fixes problem in TeamCity builds:
+
+            //The Entity Framework provider type 'System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer'
+            //for the 'System.Data.SqlClient' ADO.NET provider could not be loaded. 
+            //Make sure the provider assembly is available to the running application. 
+            //See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
     }
 }
