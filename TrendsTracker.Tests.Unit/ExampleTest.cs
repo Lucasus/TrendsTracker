846ad07d4a531a120cfc9c44eff7e41ff6685aca
Łukasz Wiatrak <lukasz.wiatrak@gmail.com>
2014-03-03 13:45:59 +0100

    Example unit test

---
+++ TrendsTracker.Tests.Unit/ExampleTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrendsTracker.Tests.Unit
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            Assert.AreEqual(2, 2, "Example assertion");
+        }
+    }
+}
